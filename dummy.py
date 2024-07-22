8daf4dc41f36f4f5b9c9af8d04d67a6ee792dec4
Ekagra Gupta <ekagrag99@gmail.com>
2024-07-22 10:00:02 +0200

    updated vif function

--- dummy.py
+++ dummy.py
@@ -3,13 +3,18 @@ from torchvision import datasets, transforms
 from utils.dataset import load_dataset
 
 transform = transforms.Compose(
-    [
-        # transforms.TrivialAugmentWide(),
-        transforms.ToTensor(),
+    [   
+        transforms.Resize(512),
         transforms.TrivialAugmentWide(),
+        transforms.ToTensor(),
+        # transforms.TrivialAugmentWide(),
     ]
 )
 
-trainloader, testloader, classes = load_dataset(batch_size=1000, transform=transform)
+trainloader, testloader, classes = load_dataset(batch_size=1, transform=transform)
 
 images, labels = next(iter(trainloader))
+
+to_pil = transforms.ToPILImage()
+pil_im = to_pil(images[0])
+pil_im.save('/home/ekagra/Documents/GitHub/MasterArbeit/example/augmented_image.png')
--- utils/vif.py
+++ utils/vif.py
@@ -1,15 +1,26 @@
 import piq
+import numpy as np
 from torchvision import transforms
+from PIL import Image
+
+def compute_vif(im1_tensor, im2_tensor):
+    im1_tensor /= 255.
+    im2_tensor /= 255.
+    
+    vif_value = piq.vif_p(im1_tensor, im2_tensor)
+    return np.clip(vif_value, 0., 1.)    
 
 
-def vif(original_image, augmented_image):
-    resize = transforms.Resize((41, 41))
-    # to_pil = transforms.ToPILImage()
+if __name__=='__main__':
+    im1_path = '/home/ekagra/Documents/GitHub/MasterArbeit/example/original_example_image.png'
+    im2_path = '/home/ekagra/Documents/GitHub/MasterArbeit/example/augmented_image.png'
+
     to_tensor = transforms.ToTensor()
-    # im_resize = to_tensor(resize(to_pil(original_image)))
-    # augment_im_resize = to_tensor(resize(to_pil(augmented_image)))
-    im_resize = to_tensor(resize(original_image))
-    augment_im_resize = to_tensor(resize(augmented_image))
-    vif_value = piq.vif_p(im_resize.unsqueeze(0), augment_im_resize.unsqueeze(0))
-    # print(f"VIF: {vif_value}")
-    return vif_value
+
+    im1_tensor = to_tensor(Image.open(im1_path)).unsqueeze(0)
+    im2_tensor = to_tensor(Image.open(im2_path)).unsqueeze(0)
+    print(im1_tensor.shape)
+
+    vif_val = compute_vif(im1_tensor, im2_tensor)
+    print(vif_val)
+
