9c8589afbdc8f8b72a2d9e4a2d55df7cbe3a20dc
Ekagra Gupta <ekagrag99@gmail.com>
2024-07-29 10:49:07 +0200

    soft loss alternative

--- compute_loss.py
+++ compute_loss.py
@@ -3,35 +3,35 @@ from torch.nn import functional as F
 import torch.nn as nn
 
 
-# def soft_loss(pred, label, confidence):
-#     log_prob = F.log_softmax(pred, dim=1)
-#     n_class = pred.size(1)
-
-#     # Make soft one-hot target
-#     label = label.unsqueeze(1)
-#     confidence = confidence.unsqueeze(1).float()
-#     one_hot = torch.ones_like(pred) * (1 - confidence) / (n_class - 1)
-#     # print(f"\nOnehot: {one_hot}\n")
-#     one_hot.scatter_(dim=1, index=label, src=confidence)
-#     # print(f"\nSoftened: {one_hot}\nSum: {torch.sum(one_hot)}")
-
-#     # Compute weighted KL loss
-#     kl = confidence * F.kl_div(input=log_prob, target=one_hot, reduction="none").sum(-1)
-#     return kl.mean()
-
 def soft_loss(pred, label, confidence):
-    label = label.unsqueeze(1)
-    target = label.long()
-    confidence = confidence.unsqueeze(1).float()
-    prob = (1 - (label - target))
-    weight = torch.ones_like(prob).float()
-    n_classes = pred.size(1)
-    
-    one_hot = (torch.ones_like(pred) * (1 - confidence) / (n_classes - 1)).float()
-    one_hot.scatter_(dim=1, index=target, src=confidence)
     log_prob = F.log_softmax(pred, dim=1)
-    kl = F.kl_div(input=log_prob.float(), target=one_hot.float(), reduction='none').sum(-1)
+    n_class = pred.size(1)
+
+    # Make soft one-hot target
+    label = label.unsqueeze(1)
+    confidence = confidence.unsqueeze(1).float()
+    one_hot = torch.ones_like(pred) * (1 - confidence) / (n_class - 1)
+    # print(f"\nOnehot: {one_hot}\n")
+    one_hot.scatter_(dim=1, index=label, src=confidence)
+    # print(f"\nSoftened: {one_hot}\nSum: {torch.sum(one_hot)}")
+
+    # Compute weighted KL loss
+    kl = confidence * F.kl_div(input=log_prob, target=one_hot, reduction="none").sum(-1)
     return kl.mean()
+
+# def soft_loss(pred, label, confidence):
+#     label = label.unsqueeze(1)
+#     target = label.long()
+#     confidence = confidence.unsqueeze(1).float()
+#     prob = (1 - (label - target))
+#     weight = torch.ones_like(prob).float()
+#     n_classes = pred.size(1)
+    
+#     one_hot = (torch.ones_like(pred) * (1 - confidence) / (n_classes - 1)).float()
+#     one_hot.scatter_(dim=1, index=target, src=confidence)
+#     log_prob = F.log_softmax(pred, dim=1)
+#     kl = F.kl_div(input=log_prob.float(), target=one_hot.float(), reduction='none').sum(-1)
+#     return kl.mean()
 
 
 if __name__ == "__main__":
