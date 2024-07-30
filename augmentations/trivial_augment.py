99f4d45f9ca39d44732a2b7e1e6030d2d33f5d9c
Ekagra Gupta <ekagrag99@gmail.com>
2024-07-30 10:32:08 +0200

    added another metric, structural similarity

--- augmentations/trivial_augment.py
+++ augmentations/trivial_augment.py
@@ -74,5 +74,5 @@ class CustomTrivialAugmentWide:
             # confidence_aa = sift_correction_factor(original_image=im, augmented_image=augment_im)
             # confidence_aa = orb_correction_factor(original_image=im, augmented_image=augment_im)
             confidence_aa = ssim_operation(im1=im, im2=augment_im)
-        print(f"\nAugmentation info: {im_info}\tconf: {confidence_aa}\n")
+        # print(f"\nAugmentation info: {im_info}\tconf: {confidence_aa}\n")
         return augment_im, confidence_aa
