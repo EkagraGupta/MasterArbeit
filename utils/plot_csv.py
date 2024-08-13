0b64fd27f5ad1f44430d2f9841e54cbbdfd5ba24
Ekagra Gupta <ekagrag99@gmail.com>
2024-08-13 14:56:45 +0200

    different image comparison metrics implemented and saved

--- utils/plot_csv.py
+++ utils/plot_csv.py
@@ -21,34 +21,29 @@ def plot_mean_and_accuracy(file_paths):
     #     plt.plot(values["Severity"], values["Mean"], label=f"k={k}")
     # plt.plot(data[1]["Severity"], data[1]["Mean"], label="psnr")
     plt.plot(data[1]["Severity"], data[1]["Mean"], label="scc")
-    plt.plot(data[2]["Severity"], data[2]["Mean"], label="ssim")
+    plt.plot(data[2]["Severity"], data[2]["Mean"], label="lightning ssim")
     plt.plot(data[3]["Severity"], data[3]["Mean"], label="uiq")
     plt.plot(data[4]["Severity"], data[4]["Mean"], label="ncc")
+    plt.plot(data[5]["Severity"], data[5]["Mean"], label="ssim")
 
     plt.plot(severity, accuracy, label="Model Accuracy",
              linestyle="--", color="black")
 
     plt.xlabel("Severity")
     plt.ylabel("Value")
-    plt.title("Mean and Accuracy Curves for Different k Values")
+    plt.title("Mean for Different Comparison Metrics")
     plt.legend()
     plt.show()
 
 
 # Example usage
 if __name__ == "__main__":
-    # file_paths = [
-    #     '/home/ekagra/Documents/GitHub/MasterArbeit/non_linear_mapping_data/TranslateX_k1_results.csv',
-    #     '/home/ekagra/Documents/GitHub/MasterArbeit/non_linear_mapping_data/TranslateX_k2_results.csv',
-    #     '/home/ekagra/Documents/GitHub/MasterArbeit/non_linear_mapping_data/TranslateX_k3_results.csv',
-    #     '/home/ekagra/Documents/GitHub/MasterArbeit/non_linear_mapping_data/TranslateX_k4_results.csv'
-    # ]
-    augmentation_type = 'TranslateX'
+    augmentation_type = 'Solarize'
     file_paths = [
-        # "/home/ekagra/Documents/GitHub/MasterArbeit/non_linear_mapping_data/Sharpness/Sharpness_l_psnr_results.csv",
         f"/home/ekagra/Documents/GitHub/MasterArbeit/non_linear_mapping_data/{augmentation_type}/{augmentation_type}_l_scc_results.csv",
         f"/home/ekagra/Documents/GitHub/MasterArbeit/non_linear_mapping_data/{augmentation_type}/{augmentation_type}_l_ssim_results.csv",
         f"/home/ekagra/Documents/GitHub/MasterArbeit/non_linear_mapping_data/{augmentation_type}/{augmentation_type}_l_uiq_results.csv",
-        f"/home/ekagra/Documents/GitHub/MasterArbeit/non_linear_mapping_data/{augmentation_type}/{augmentation_type}_ncc_results.csv"
+        f"/home/ekagra/Documents/GitHub/MasterArbeit/non_linear_mapping_data/{augmentation_type}/{augmentation_type}_ncc_results.csv",
+        f"/home/ekagra/Documents/GitHub/MasterArbeit/non_linear_mapping_data/{augmentation_type}/{augmentation_type}_ssim_results.csv"
     ]
     plot_mean_and_accuracy(file_paths)
