af021f9ff9c0a875cbd0f52ab7145324af2603eb
Ekagra Gupta <ekagrag99@gmail.com>
2024-09-18 14:01:51 +0200

    conf<0.5=1.0

--- augment_dataset.py
+++ augment_dataset.py
@@ -283,7 +283,7 @@ def display_image_grid(images, labels, confidences, batch_size, classes):
 
 
 if __name__ == "__main__":
-    batch_size = 20
+    batch_size = 10
     DATASET_NAME = "CIFAR10"
     transforms_preprocess, transforms_augmentation = create_transforms(
         random_cropping=False,
