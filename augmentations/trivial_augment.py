bf3ce7675ab186de8d27c7a74a2a9d278e5a511a
Ekagra Gupta <ekagrag99@gmail.com>
2024-10-04 10:09:58 +0200

    added hvs curves for contrast, adjusted same curve for brightness, sharpness and color

--- augmentations/trivial_augment.py
+++ augmentations/trivial_augment.py
@@ -295,13 +295,13 @@ class CustomTrivialAugmentWide(torch.nn.Module):
             #     augmentation_magnitude, 0.9753, 17.0263, -0.8297
             # )
             # confidence_aa = model_accuracy_mapping(augmentation_magnitude, augmentation_type)
-            k = 3  # 2, 6
+            # k = 3  # 2, 6
             chance = 0.102  # 0.102, 0.1
             # confidence_aa = get_data(abs(augmentation_magnitude), k=k, chance=0.1)
             # confidence_aa = 1.0 if augmentation_magnitude>0.0 else confidence_aa
             augmentation_magnitude = (augmentation_magnitude + 1.0) / 2.0
             k = 10
-            chance = 0.32
+            # chance = 0.32
             confidence_aa = get_data(augmentation_magnitude, k=k, chance=chance)
         elif augmentation_type == "Contrast":  # HVS Available
             # confidence_aa = comparison_metrics.sigmoid(
@@ -317,26 +317,26 @@ class CustomTrivialAugmentWide(torch.nn.Module):
             #     augmentation_magnitude, 1.0, 4.93537641, -1.5837580
             # )
             # confidence_aa = model_accuracy_mapping(augmentation_magnitude, augmentation_type)
-            k = 3  # 2, 6
+            # k = 3  # 2, 6
             chance = 0.95  # 0.95, 0.1
             # confidence_aa = get_data(abs(augmentation_magnitude), k=k, chance=chance)
             # confidence_aa = 1.0 if augmentation_magnitude>0.0 else confidence_aa
             augmentation_magnitude = (augmentation_magnitude + 1.0) / 2.0
             k = 10
-            chance = 0.32
+            # chance = 0.32
             confidence_aa = get_data(augmentation_magnitude, k=k, chance=chance)
         elif augmentation_type == "Sharpness":
             # confidence_aa = comparison_metrics.sigmoid(
             #     augmentation_magnitude, 0.9995181, 7.07685057, -1.24349678
             # )
             # confidence_aa = model_accuracy_mapping(augmentation_magnitude, augmentation_type)
-            k = 3  # 2, 8
+            # k = 3  # 2, 8
             chance = 0.884  # 0.95, 0.1
             # confidence_aa = get_data(abs(augmentation_magnitude), k=k, chance=chance)
             # confidence_aa = 1.0 if augmentation_magnitude>0.0 else confidence_aa
             augmentation_magnitude = (augmentation_magnitude + 1.0) / 2.0
             k = 10
-            chance = 0.32
+            # chance = 0.32
             confidence_aa = get_data(augmentation_magnitude, k=k, chance=chance)
         # elif augmentation_type == "Posterize":
         # confidence_aa = comparison_metrics.multiscale_structural_similarity(
