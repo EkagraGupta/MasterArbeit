7f8f101fc90f4b9cbf7b31d654ce863345f5eb92
Ekagra Gupta <ekagrag99@gmail.com>
2024-10-23 16:03:40 +0200

    tiny-imagenet added

--- augment_dataset.py
+++ augment_dataset.py
@@ -200,8 +200,8 @@ def load_data(
         base_trainset = datasets.CIFAR100(root="./data/train", train=True, download=True)
         base_testset = datasets.CIFAR100(root="./data/test", train=False, download=True)
     elif dataset_name == "Tiny-ImageNet":
-        base_trainset = datasets.ImageFolder(root="/kaggle/working/MasterArbeit/data/tiny-imagenet-200/new_train")
-        base_testset = datasets.ImageFolder(root="/kaggle/working/MasterArbeit/data/tiny-imagenet-200/new_test")
+        base_trainset = datasets.ImageFolder(root="./data/tiny-imagenet-200/new_train")
+        base_testset = datasets.ImageFolder(root="./data/tiny-imagenet-200/new_test")
     else:
         raise ValueError(f"Dataset {dataset_name} not supported")
 
