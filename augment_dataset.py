ef9638a77af7acaa744d80f5df3580de52ae1f16
Ekagra Gupta <ekagrag99@gmail.com>
2024-11-16 10:27:45 +0100

    koverestimate

--- augment_dataset.py
+++ augment_dataset.py
@@ -131,7 +131,7 @@ def create_transforms(
     t = [transforms.ToTensor()]
     augmentations = [
         transforms.RandomHorizontalFlip(),
-        transforms.RandomCrop(64, padding=4),       # For Tiny-ImageNet: 64 x 64; For CIFAR: 32 x 32
+        transforms.RandomCrop(32, padding=4),       # For Tiny-ImageNet: 64 x 64; For CIFAR: 32 x 32
         # transforms.TrivialAugmentWide(),
         # transforms.Resize(256),
     ]
