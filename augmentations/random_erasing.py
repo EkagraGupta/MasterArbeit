4ff7ae036631f264208661fff8dfbe6e4e5c5f0b
Ekagra Gupta <ekagrag99@gmail.com>
2024-12-06 12:25:41 +0100

    k=1

--- augmentations/random_erasing.py
+++ augmentations/random_erasing.py
@@ -60,7 +60,7 @@ class RandomErasing(torch.nn.Module):
         self.inplace = inplace
         """MODIFICATION"""
         self.dataset_name = dataset_name
-        self.k = 2
+        self.k = 1
         self.custom = custom
         if self.dataset_name == 'CIFAR10':
             self.chance = 0.1
